1279490ef27c3df17cf9feb3e1733d08805590d5
alfieqashwa <alfieqashwa@gmail.com>
2020-03-20 10:54:22 +0700

    finished fetch Countries and Indonesia

--- components/Countries.js
+++ components/Countries.js
@@ -7,79 +7,54 @@ export default () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>ERROR!</p>;
 
-  console.log(data);
+  // cases: 41035
+  // casesPerOneMillion: 679
+  // country: "Italy"
+  // critical: 2498
+  // deaths: 3405
+  // recovered: 4440
+  // todayCases: 0
+  // todayDeaths: 0
+  // console.log(data);
   return (
-    <div className="flex flex-col">
-      <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
-        <div className="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
-          <table className="min-w-full">
-            <thead>
-              <tr>
-                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                  Name
-                </th>
-                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                  Title
-                </th>
-                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                  Status
-                </th>
-                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                  Role
-                </th>
-                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
-              </tr>
-            </thead>
-            <tbody className="bg-white">
-              <tr>
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                  <div className="flex items-center">
-                    <div className="flex-shrink-0 h-10 w-10">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </div>
-                    <div className="ml-4">
-                      <div className="text-sm leading-5 font-medium text-gray-900">
-                        Bernard Lane
-                      </div>
-                      <div className="text-sm leading-5 text-gray-500">
-                        bernardlane@example.com
-                      </div>
-                    </div>
-                  </div>
-                </td>
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                  <div className="text-sm leading-5 text-gray-900">
-                    Director
-                  </div>
-                  <div className="text-sm leading-5 text-gray-500">
-                    Human Resources
-                  </div>
-                </td>
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                  <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                    Active
-                  </span>
-                </td>
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
-                  Owner
-                </td>
-                <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                  <a
-                    href="#"
-                    className="text-indigo-600 hover:text-indigo-900 focus:outline-none focus:underline"
-                  >
-                    Edit
-                  </a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+    <>
+      <div className="w-full md:w-1/2 xl:w-1/3 pt-3 px-3 md:pr-2">
+        <button
+          onClick={refetch}
+          className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 border border-teal-700 rounded"
+        >
+          RELOAD COUNTRIES
+        </button>
       </div>
-    </div>
+      <div className="flex flex-wrap mb-2">
+        {data.map((c, i) => (
+          <div
+            key={i}
+            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 pt-3 px-3 mb-4"
+          >
+            <div className="bg-teal-600 border rounded shadow p-2">
+              <div className="flex flex-row items-center">
+                <div className="flex-shrink pl-1 pr-4">
+                  <i className="fa fa-wallet fa-2x fa-fw fa-inverse" />
+                </div>
+                <div className="flex-1 text-left">
+                  <h5 className="text-white text-2xl ">{c.country}</h5>
+                  <h3 className="text-white text-1xl">Kasus: {c.cases}</h3>
+                  <h3 className="text-white text-1xl">Kritis: {c.critical}</h3>
+                  <h3 className="text-white text-1xl">Meninggal: {c.deaths}</h3>
+                  <h3 className="text-white text-1xl">Pulih: {c.recovered}</h3>
+                  <h3 className="text-white text-1xl">
+                    Kasus Hari ini: {c.todayCases}
+                  </h3>
+                  <h3 className="text-white text-1xl">
+                    Meninggal Hari ini: {c.todayDeaths}
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
--- pages/indonesia.js
+++ pages/indonesia.js
@@ -1,9 +1,118 @@
+import useAxios from "axios-hooks";
+import { BASE_URL } from "../components/BaseUrl";
 import Layout from "../components/Layout";
 
 export default () => {
   return (
     <Layout>
       <h1>Indonesia</h1>
+      <Indonesia />
     </Layout>
   );
 };
+
+function Indonesia() {
+  const [{ data, loading, error }, refetch] = useAxios(
+    `${BASE_URL}/countries/indonesia`
+  );
+
+  if (loading) return <p>Loading... </p>;
+  if (error) return <p>ERROR!</p>;
+
+  // cases: 41035
+  // casesPerOneMillion: 679
+  // country: "Italy"
+  // critical: 2498
+  // deaths: 3405
+  // recovered: 4440
+  // todayCases: 0
+  // todayDeaths: 0
+  //
+  const { cases, deaths, recovered, critical, todayCases, todayDeaths } = data;
+  return (
+    <>
+      <div className="flex flex-wrap mb-2">
+        <div className="w-full md:w-1/2 xl:w-1/3 pt-3 px-3 md:pr-2">
+          <div className="bg-red-600 border rounded shadow p-2">
+            <div className="flex flex-row items-center">
+              <div className="flex-shrink pl-1 pr-4">
+                <i className="fa fa-wallet fa-2x fa-fw fa-inverse" />
+              </div>
+              <div className="flex-1 text-center">
+                <h5 className="text-white text-3xl font-bold">Kasus</h5>
+                <h3 className="text-white text-4xl">
+                  {cases}
+                  <span className="text-green-400">
+                    <i className="fas fa-caret-down" />
+                  </span>
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 xl:w-1/3 pt-3 px-3 md:pl-2">
+          <div className="bg-black-600 border rounded shadow p-2">
+            <div className="flex flex-row items-center">
+              <div className="flex-shrink pl-1 pr-4">
+                <i className="fas fa-users fa-2x fa-fw fa-inverse" />
+              </div>
+              <div className="flex-1 text-center">
+                <h5 className="text-white text-3xl font-bold">Meninggal</h5>
+                <h3 className="text-white text-4xl">
+                  {deaths}
+                  <span className="text-blue-400">
+                    <i className="fas fa-caret-up" />
+                  </span>
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 xl:w-1/3 pt-3 px-3 md:pr-2 xl:pr-3 xl:pl-1">
+          <div className="bg-green-600 border rounded shadow p-2">
+            <div className="flex flex-row items-center">
+              <div className="flex-shrink pl-1 pr-4">
+                <i className="fas fa-user-plus fa-2x fa-fw fa-inverse"></i>
+              </div>
+              <div className="flex-1 text-center">
+                <h5 className="text-white text-3xl font-bold">Pulih</h5>
+                <h3 className="text-white text-4xl">
+                  {recovered}
+                  <span className="text-orange-400">
+                    <i className="fas fa-caret-up"></i>
+                  </span>
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 xl:w-1/3 pt-3 px-3 md:pr-2 xl:pr-3 xl:pl-1">
+          <div className="bg-orange-600 border rounded shadow p-2">
+            <div className="flex flex-row items-center">
+              <div className="flex-shrink pl-1 pr-4">
+                <i className="fas fa-user-plus fa-2x fa-fw fa-inverse"></i>
+              </div>
+              <div className="flex-1 text-center">
+                <h5 className="text-white text-3xl font-bold">Kritis</h5>
+                <h3 className="text-white text-4xl">
+                  {critical}
+                  <span className="text-orange-400">
+                    <i className="fas fa-caret-up"></i>
+                  </span>
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full md:w-1/2 xl:w-1/3 pt-3 px-3 md:pr-2">
+        <button
+          onClick={refetch}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+        >
+          RELOAD
+        </button>
+      </div>
+    </>
+  );
+}
