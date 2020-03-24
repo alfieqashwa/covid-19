668d40e8e4fc8459e6cdfee162395fc2fc0af54c
alfieqashwa <alfieqashwa@gmail.com>
2020-03-24 16:53:37 +0700

    added DATA SOURCE API on footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,7 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
 const Footer = () => (
   <footer className="bg-gray-900 border-t border-gray-400 rounded shadow mx-2 p-6">
     <div className="container mx-auto flex py-8">
@@ -6,6 +10,18 @@ const Footer = () => (
           <div className="px-3">
             <h3 className="font-bold font-bold text-gray-100">Resources</h3>
             <ul className="list-reset items-center text-sm pt-3">
+              <li>
+                <a
+                  className="inline-block text-gray-600 no-underline hover:text-gray-100 hover:text-underline py-1"
+                  href="https://github.com/NovelCOVID/API"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span className="text-gray-600 font-bold">
+                    DATA SOURCE API
+                  </span>
+                </a>
+              </li>
               <li>
                 <a
                   className="inline-block text-gray-600 no-underline hover:text-gray-100 hover:text-underline py-1"
@@ -40,9 +56,12 @@ const Footer = () => (
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  This non-profit site is crafted by{" "}
+                  This non-profit site is made with{" "}
+                  <span className="text-red-700">
+                    <FontAwesomeIcon icon={faHeart} />
+                  </span>{" "}
+                  by{" "}
                   <span className="text-gray-600 font-bold">Alfie Qashwa</span>,
-                  Indonesia
                 </a>
               </li>
             </ul>
